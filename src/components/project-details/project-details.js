dc0e5a275317210f752da08726aac73a8d69be8a
VeiaG <romapalamar.veiag@gmail.com>
2024-02-20 23:38:17 +0200

    added more projects , fixed some bugs

--- src/components/project-details/project-details.js
+++ src/components/project-details/project-details.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./project-details.scss";
 import { useNavigate, useParams, } from "react-router-dom";
 import { useEffect, useState } from "react";
+import FullscreenImage from "../fullscreen-image/fullscreen-image";
 import Error from "../error/error";
 const ProjectDetails = ({data}) => {
   const [item,setItem]=useState(undefined);
@@ -18,15 +19,16 @@ const ProjectDetails = ({data}) => {
   const navigate = useNavigate();
   const images = item?.images?.map((image, index) => {
     return (
-      <img
-        key={index}
-        src={image}
-        alt={item?.name}
-        className="details__image"
-      />
+      <FullscreenImage key={index}><img
+          key={index}
+          src={image}
+          alt={item?.name}
+          className="details__image"
+        /></FullscreenImage>
+      
     );
   })
-  return item ? (<div className="details">
+  return data ? (<div className="details">
   <div className="details__wrapper">
     <div className="details__back" onClick={() => window.history.back()}>
       <i className="fas fa-arrow-left"></i>
@@ -44,14 +46,20 @@ const ProjectDetails = ({data}) => {
             })
           }
         </div>
+       <div className="details__links">
+        {item?.sourceURL ? (<a href={item.sourceURL}><i className="fa-brands fa-github"></i></a>) : null}
+        {item?.figmaURL ? (<a href={item.figmaURL}><i className="fa-brands fa-figma"></i></a>) : null}
+       </div>
         <a href={item?.url} className="btn">Переглянути</a>
       </div>
     </div>
-    <h2>Галерея</h2>
-    <div className="details__gallery">
-        {images}
-    </div>
+    {item?.images.length >0 ? (<>
+      <h2>Галерея</h2>
+      <div className="details__gallery">
+          {images}
+      </div>
+    </>) :null}
   </div>
-</div>): !isLoading ? <Error msg="Проєкт не знайдено" title="Помилка!"/> : '';
+</div>): isLoading ? <Error msg="Проєкт не знайдено" title="Помилка!"/> : '';
 }
 export default ProjectDetails;
